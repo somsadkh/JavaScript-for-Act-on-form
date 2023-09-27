24e9ea20d15ce3516eef48c5c4551fb3013c53de
somsadkh <somsad.kh@gmail.com>
2023-09-27 17:07:53 -0400

    final update

--- custom.js
+++ custom.js
@@ -1,10 +1,13 @@
-$(".table-trending .fa-thumbs-down").click(function(){
-	// $("#b1695214385005").remove()
-    $("#b1695214244627").remove()
-	var element = document.createElement("input");
-	element.type = "hidden";
-	element.name = "Line Item Reject";
-	element.id = "b1695214244627";
-	element.value=$(this).parents("td").attr('value')
-	document.getElementById("ao-form-f20b3824-1f32-49a4-9aa3-628cc8b95c5c").appendChild(element); 
+$(".table-trending .fa-thumbs-down").click(function () {
+    // $("#b1695214385005").remove()
+    $("#b1695214385005").val('')
+    $("#b1695214244627").val($(this).parents("td").attr('value'))
+})
+
+$(".table-trending .fa-thumbs-up").click(function () {
+    // $("#b1695214244627").remove()
+    $("#b1695214244627").val('')
+    $("#b1695214385005").val($(this).parents("td").attr('value'));
+
+    $('#ao-form-f20b3824-1f32-49a4-9aa3-628cc8b95c5c').submit();
 })
